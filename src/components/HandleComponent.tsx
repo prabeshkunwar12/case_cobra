f1f80772258150f77be556e1f9c09bc7ff3ba8b5
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-05-10 13:15:57 -0300

    Design Configurator for resizing image

---
+++ src/components/HandleComponent.tsx
@@ -0,0 +1,7 @@
+const HandleComponent = () => {
+    return (
+        <div className="w-5 h-5 bg-white rounded-full shadow border border-zinc-200 transistion hover:bg-primary" />
+    )
+}
+
+export default HandleComponent
