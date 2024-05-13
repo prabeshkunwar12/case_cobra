b160ce0ca30e9557c373555329a1249b5423bba0
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-05-13 15:51:23 -0300

    changed font to recursive and bg to grainy light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Recursive } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const recursive = Recursive({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -20,17 +20,17 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={recursive.className}>
 				<Navbar />
-					<main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
-						<div className="flex flex-1 flex-col h-full">
-							<Providers>
-								{children}
-							</Providers>
-						</div>
-						<Footer /> 
-					</main>
-					<Toaster />
+				<main className="flex flex-col grainy-light min-h-[calc(100vh-3.5rem-1px)]">
+					<div className="flex flex-1 flex-col h-full">
+						<Providers>
+							{children}
+						</Providers>
+					</div>
+					<Footer /> 
+				</main>
+				<Toaster />
 			</body>
 		</html>
 	);
