8e58e70edfc50395efe5c10c4ddd873ad294b831
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-05-15 16:42:10 -0300

    webhook for stripe

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,5 +1,7 @@
+import { db } from "@/db"
 import { stripe } from "@/lib/stripe"
 import { headers } from "next/headers"
+import { NextResponse } from "next/server"
 import Stripe from "stripe"
 
 export async function POST(req: Request){
@@ -21,12 +23,65 @@ export async function POST(req: Request){
             if(!event.data.object.customer_details?.email) {
                 throw new Error('Missing user email')
             }
+
+            const session = event.data.object as Stripe.Checkout.Session
+
+            const { userId, orderId } = session.metadata || {
+                userId: null,
+                orderId: null,
+            }
+
+            if(!userId || !orderId) {
+                throw new Error("Invalid request metadata")
+            }
+
+            const billingAddress = session.customer_details!.address
+            const shippingAddress = session.shipping_details!.address
+            
+            await db.order.update({
+                where: {
+                    id: orderId,
+                    userId: userId,
+                },
+                data: {
+                    isPaid: true,
+                    shippingAddress: {
+                        create: {
+                            name: session.customer_details!.name!,
+                            city: shippingAddress!.city!,
+                            country: shippingAddress!.country!,
+                            postalCode: shippingAddress!.postal_code!,
+                            street: shippingAddress!.line1!,
+                            state: shippingAddress!.state,
+                        }
+                    },
+                    billingAddress: {
+                        create: {
+                            name: session.customer_details!.name!,
+                            city: billingAddress!.city!,
+                            country: billingAddress!.country!,
+                            postalCode: billingAddress!.postal_code!,
+                            street: billingAddress!.line1!,
+                            state: billingAddress!.state,
+                        }
+                    },
+                }
+            })
         }
 
-        const session = event.data.object as Stripe.Checkout.Session
-
-        const {}
+        return NextResponse.json({ result: event, ok: true })
     } catch (err) {
+        console.error(err)
 
+        //send this sentry if in entreprise
+
+        return NextResponse.json(
+            {
+                message: 'Something went wrong', ok:false
+            },
+            {
+                status:500
+            }
+        )
     }
 }
