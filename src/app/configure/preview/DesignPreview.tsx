d7fef105de4c5261a8f153fca07b3aa48448a515
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-05-16 18:02:13 -0300

    updated design preview

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -73,15 +73,15 @@ const DesignPreview = ({configuration}:{configuration: Configuration}) => {
 
             <LoginModel isOpen={isLoginModelOpen} setIsOpen={setIsLoginModelOpen} />
             
-            <div className='mt-20 text-sm grid grid-cols-1 sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
-                <div className='sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2'>
+            <div className='mt-20 flex flex-col items-center text-sm md:grid sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
+                <div className='md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2'>
                     <Phone 
-                        className={cn(`bg-${tw}`)} 
+                        className={cn(`bg-${tw}`, "max-w-[150px] md:max-w-full")} 
                         imgSrc={configuration.croppedImageUrl!} 
                     />
                 </div>
 
-                <div className='mt-6 sm:col-span-9 sm:mt-0 md:row-end-1'>
+                <div className='mt-6 sm:col-span-9 md:row-end-1'>
                     <h3 className='text-3xl font-bold tracking-tight text-gray-900'> Your {modelLabel} Case</h3>
                     <div className='mt-3 flex items-center gap-1.5 text-base'>
                         <Check className='h-4 w-4 text-green-500' />
