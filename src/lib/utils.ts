2ae575ada7dbdb4cd29274992445279f0853d619
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-05-16 18:18:52 -0300

    Changed metadata and favicon

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,46 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  	return twMerge(clsx(inputs))
 }
 
 export const formatPrice = (price: number, currency: string) => {
-  const formatter = new Intl.NumberFormat("en-us", {
-    style:"currency",
-    currency:currency,
-  })
+	const formatter = new Intl.NumberFormat("en-us", {
+		style:"currency",
+		currency:currency,
+	})
 
-  return formatter.format(price)
+	return formatter.format(price)
+}
+
+export const constructMetadata = ({
+	title = "CaseCobra - Custom high-quality phone cases",
+	description = "create custom high-quality phone cases in seconds",
+	image = "/thumbnail.png",
+	icons = "/favicon.ico"
+}:{
+	title?: string
+	description?: string
+	image?: string
+	icons?: string
+} = {}): Metadata => {
+	return {
+		title,
+		description,
+		openGraph: {
+			title,
+			description,
+			images: [{url:image}]
+		},
+		twitter: {
+			card: "summary_large_image",
+			title,
+			description,
+			images: [image],
+			creator: "@prabeshkunwar12"
+		},
+		icons
+	}
 }
